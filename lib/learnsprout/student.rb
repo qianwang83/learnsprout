226492fea23a70e09e06aac1065de6cba8103495
Qian Wang <qian.wang83@gmail.com>
2013-08-05 10:48:00 -0700

    print lines for debugging

--- lib/learnsprout/student.rb
+++ lib/learnsprout/student.rb
@@ -36,6 +36,8 @@ module LearnSprout
       @gender = attrs["gender"]
       @exit_date = attrs["exit_date"] && Date.parse(attrs["exit_date"])
       @entry_date = attrs["entry_date"] && Date.parse(attrs["entry_date"])
+      puts "here"
+      puts "extras #{attrs['extras']}"
       @enroll_status = attrs["extras"]["enroll_status"]
     end
   end
